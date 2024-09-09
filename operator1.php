c058b2bfb1178568bbe4b920c7aa39153ccc8718
AbdullahMuchsin <abdullahmuchsin96@gmail.com>
2024-09-09 07:54:08 +0700

    UPDATE -Menambahkan kode dan file

--- operator1.php
+++ operator1.php
@@ -1,9 +1,11 @@
 <?php
-$tugas1=90;
-$tugas2=80;
-$jumlah=$tugas1 + $tugas2;
-$rerata=$jumlah/2 ;
-echo "Nilai Tugas I : ".$tugas1." <br>" . PHP_EOL;
-echo "Nilai Tugas II: ".$tugas2." <br>" . PHP_EOL;
-echo "Jumlah Tugas : ".$jumlah." <br>" . PHP_EOL;
-echo "Rerata Tugas : ".$rerata . PHP_EOL;
+$tugas1 = 90;
+$tugas2 = 80;
+$jumlah = $tugas1 + $tugas2;
+$jumlahTugas3 = $tugas1 . $tugas2;
+$rerata = $jumlah / 2;
+echo "Nilai Tugas I : " . $tugas1 . " <br>" . PHP_EOL;
+echo "Nilai Tugas II: " . $tugas2 . " <br>" . PHP_EOL;
+echo "Jumlah Tugas : " . $jumlah . " <br>" . PHP_EOL;
+echo "Rerata Tugas : " . $rerata . PHP_EOL;
+echo "Hasil Jumlah Tugas 3 : " . $jumlahTugas3 . PHP_EOL;
